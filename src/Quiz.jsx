a6ec1d27ea90b8f89a22da96179d2741a8d74418
bklynate <nathanielcfa@gmail.com>
2024-03-16 13:22:48 -0400

    fix issue where quiz wasn't displaying

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -7,26 +7,36 @@ export default function Quiz() {
   const fetchQuizCategories = async () => {
     const { data } = await axios('https://the-trivia-api.com/v2/questions');
 
-      const formattedData = data.map((category) => {
-        const incorrectAnswersIndexes = category.incorrectAnswers.length 
-        const randomIndex = Math.random() * (incorrectAnswersIndexes - 0) + 0 
-        category.incorrectAnswers.splice(randomIndex, 0, category.correctAnswer)
-        return {
-          ...category,
-          answers: category.incorrectAnswers,
-        }
-      })
-    
-    setCategories(data.category);
+    const formattedData = data.map((category) => {
+      const incorrectAnswersIndexes = category.incorrectAnswers.length;
+      const randomIndex = Math.random() * (incorrectAnswersIndexes - 0) + 0;
+      category.incorrectAnswers.splice(randomIndex, 0, category.correctAnswer);
+      return {
+        ...category,
+        answers: category.incorrectAnswers,
+      }
+    })
+
+    setCategories(formattedData);
   }
 
-  useEffect(() => {
-    fetchQuizCategories()
-  }, [])
- console.log({ categories }); 
+  useEffect(() => fetchQuizCategories(), [])
+
   return (
     <Container>
-
+      <h1>Quiz</h1>
+      <div>
+        {categories.map((category, index) => (
+          <div key={index}>
+            <h3>{category.question.text}</h3>
+            <ul>
+              {category.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </Container>
   ) 
 }
